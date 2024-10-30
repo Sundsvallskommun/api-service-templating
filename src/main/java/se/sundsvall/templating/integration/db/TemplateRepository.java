2454dde7a8306da9fd85e06d33d3df5577901c40
cheezi0747 <linus.sjolinder@b3.se>
2024-10-30 15:40:51 +0100

    UF-10709: Fixes sonar issues

--- src/main/java/se/sundsvall/templating/integration/db/TemplateRepository.java
+++ src/main/java/se/sundsvall/templating/integration/db/TemplateRepository.java
@@ -1,8 +1,5 @@
 package se.sundsvall.templating.integration.db;
 
-import java.util.List;
-import java.util.Optional;
-
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,9 +9,11 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import se.sundsvall.templating.integration.db.entity.TemplateEntity;
 import se.sundsvall.templating.integration.db.entity.Version;
+
+import java.util.List;
+import java.util.Optional;
 
 @CircuitBreaker(name = "templateRepository")
 public interface TemplateRepository extends JpaRepository<TemplateEntity, String>, JpaSpecificationExecutor<TemplateEntity> {
@@ -22,23 +21,23 @@ public interface TemplateRepository extends JpaRepository<TemplateEntity, String
 	boolean existsByIdentifierAndMunicipalityId(String identifier, String municipalityId);
 
 	@Query("""
-			select case when count(t)> 0 then true else false end
-			from TemplateEntity t
-			where
-			t.identifier = :identifier and
-			t.municipalityId = :municipalityId and
-			t.version.major = :#{#version.major} and
-			t.version.minor = :#{#version.minor}
+		select case when count(t)> 0 then true else false end
+		from TemplateEntity t
+		where
+		t.identifier = :identifier and
+		t.municipalityId = :municipalityId and
+		t.version.major = :#{#version.major} and
+		t.version.minor = :#{#version.minor}
 		""")
 	boolean existsByIdentifierAndVersionAndMunicipalityId(@Param("identifier") String identifier, @Param("version") Version version, @Param("municipalityId") String municipalityId);
 
 	@Query("""
-			from TemplateEntity t
-			where
-			t.identifier = :identifier and
-			t.municipalityId = :municipalityId and
-			t.version.major = :#{#version.major} and
-			t.version.minor = :#{#version.minor}
+		from TemplateEntity t
+		where
+		t.identifier = :identifier and
+		t.municipalityId = :municipalityId and
+		t.version.major = :#{#version.major} and
+		t.version.minor = :#{#version.minor}
 		""")
 	Optional<TemplateEntity> findByIdentifierAndVersionAndMunicipalityId(
 		@Param("identifier") String identifier,
@@ -46,42 +45,42 @@ public interface TemplateRepository extends JpaRepository<TemplateEntity, String
 		@Param("municipalityId") String municipalityId);
 
 	@Query("""
-			from TemplateEntity t
-			where
-			t.municipalityId = :municipalityId
+		from TemplateEntity t
+		where
+		t.municipalityId = :municipalityId
 		""")
 	List<TemplateEntity> findAllByMunicipalityId(@Param("municipalityId") String municipalityId);
 
-	default Optional<TemplateEntity> findLatestByIdentifierAndMunicipalityId(@Param("identifier") String identifier, @Param("municipalityId") String municipalityId) {
+	default Optional<TemplateEntity> findLatestByIdentifierAndMunicipalityId(@Param("identifier") final String identifier, @Param("municipalityId") final String municipalityId) {
 		return findLatestByIdentifierAndMunicipalityId(identifier, municipalityId, PageRequest.of(0, 1)).stream().findFirst();
 	}
 
 	@Query("""
-			from TemplateEntity t
-			where
-			t.identifier = :identifier and
-			t.municipalityId = :municipalityId
-			order by t.version.major desc, t.version.minor desc
+		from TemplateEntity t
+		where
+		t.identifier = :identifier and
+		t.municipalityId = :municipalityId
+		order by t.version.major desc, t.version.minor desc
 		""")
 	Page<TemplateEntity> findLatestByIdentifierAndMunicipalityId(@Param("identifier") String identifier, @Param("municipalityId") String municipalityId, Pageable pageable);
 
 	@Modifying
 	@Query("""
-			delete from TemplateEntity t
-			where
-			t.identifier = :identifier and
-			t.municipalityId = :municipalityId
+		delete from TemplateEntity t
+		where
+		t.identifier = :identifier and
+		t.municipalityId = :municipalityId
 		""")
 	void deleteByIdentifierAndMunicipalityId(@Param("identifier") String identifier, @Param("municipalityId") String municipalityId);
 
 	@Modifying
 	@Query("""
-			delete from TemplateEntity t
-			where
-			t.identifier = :identifier and
-			t.municipalityId = :municipalityId and
-			t.version.major = :#{#version.major} and
-			t.version.minor = :#{#version.minor}
+		delete from TemplateEntity t
+		where
+		t.identifier = :identifier and
+		t.municipalityId = :municipalityId and
+		t.version.major = :#{#version.major} and
+		t.version.minor = :#{#version.minor}
 		""")
 	void deleteByIdentifierAndVersionAndMunicipalityId(@Param("identifier") String identifier, @Param("version") Version version, @Param("municipalityId") String municipalityId);
 }
