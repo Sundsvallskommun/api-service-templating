4d9a3f174766aef86a01c9f1eb172751434dc978
Michael Carlberg <michael.carlberg@b3.se>
2025-08-22 10:24:05 +0200

    Update integration test data

--- src/integration-test/resources/db/truncate.sql
+++ src/integration-test/resources/db/truncate.sql
@@ -1,5 +1,6 @@
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE `templates_metadata`;
 TRUNCATE TABLE `templates_default_values`;
+TRUNCATE TABLE `template_content`;
 TRUNCATE TABLE `templates`;
 SET FOREIGN_KEY_CHECKS = 1;
