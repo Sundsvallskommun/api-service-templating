07b6d7f1c5630382008a206264f0b0f4d5ff2004
Michael Carlberg <michael.carlberg@b3.se>
2025-08-22 10:07:13 +0200

    Replace incorrect table name

--- src/main/java/se/sundsvall/templating/integration/db/entity/TemplateContentEntity.java
+++ src/main/java/se/sundsvall/templating/integration/db/entity/TemplateContentEntity.java
@@ -15,7 +15,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "templates")
+@Table(name = "template_content")
 @Data
 @Builder(setterPrefix = "with")
 @NoArgsConstructor
